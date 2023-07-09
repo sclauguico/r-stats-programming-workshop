8229542c9d7d9dcb1d45220d7e23d91388bccb9f
Sandy Lauguico <sclauguico@gmail.com>
2023-07-09 10:12:22 +0800

    Cleaning the repository with the final files

--- 3-r-forcasting-and-business-intelligence/1-forecasting-in-r/01-time-series-in-R/01-time-series-in-R.R
+++
@@ -1,8 +0,0 @@
-# Read the data from Excel into R
-mydata <- read_excel("exercise1.xlsx")
-
-# Look at the first few lines of mydata
-head(mydata)
-
-# Create a ts object called myts
-myts <- ts(mydata[, 2:4], start = c(1981, 1), frequency = 4)
