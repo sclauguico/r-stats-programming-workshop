171d588ac43bd3f8d44e4499fce92664b7f6b6ba
Sandy Lauguico <sclauguico@gmail.com>
2023-07-14 13:16:31 +0800

    Finalized intro to regression and prediction

--- 2-r-statistics-fundamentals/2-r-intro-to-regression/01-simple-linear-regression/01-simple-linear-regression.R
+++ 2-r-statistics-fundamentals/2-r-intro-to-regression/01-simple-linear-regression/01-simple-linear-regression.R
@@ -1,9 +1,9 @@
-# Install packINDUSs 
-install.packINDUSs("tidyverse")
-# install.packINDUSs("dplyr")
-# install.packINDUSs("magrittr")
-# install.packINDUSs("ggplot2")
-# install.packINDUSs("lubridate")
+# Install.packages 
+install.packages("tidyverse")
+# install.packages("dplyr")
+# install.packages("magrittr")
+# install.packages("ggplot2")
+# install.packages("lubridate")
 
 library(tidyverse) # for data tidying
 library(corrplot) # for corrlation plots
@@ -46,24 +46,24 @@ corrplot(cor(boston_real_estate_clean), method = "circle", type = "upper",
 
 
 # Add a linear trend line without a confidence ribbon
-ggplot(boston_real_estate, aes(INDUS, TAX)) +
+ggplot(boston_real_estate_clean, aes(INDUS, TAX)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "lm", se = FALSE)
 
 
 # Run a linear regression of TAX vs. INDUS
-lm(TAX ~ INDUS, data = boston_real_estate)
+lm(TAX ~ INDUS, data = boston_real_estate_clean)
 
 
 
 # CATEGORICAL INDEPENDENT VARIABLES
-boston_real_estate <- boston_real_estate %>%
+boston_real_estate_clean <- boston_real_estate_clean %>%
   mutate(INDUS_group = cut(INDUS, breaks = seq(0, 30, 5), include.lowest = TRUE, labels = FALSE)) %>%
   mutate(INDUS_group = factor(INDUS_group, labels = c("0-5", "6-10", "11-15", "16-20", "21-25", "26-30")))
 
 
 # Using boston_real_estate, plot TAX
-ggplot(boston_real_estate, aes(TAX)) +
+ggplot(boston_real_estate_clean, aes(TAX)) +
   # Make it a histogram with 10 bins
   geom_histogram(bins = 10) +
   # Facet the plot so each house INDUS group gets its own panel
@@ -71,7 +71,7 @@ ggplot(boston_real_estate, aes(TAX)) +
 
 
 
-summary_stats <- boston_real_estate %>% 
+summary_stats <- boston_real_estate_clean %>% 
   # Group by INDUS
   group_by(INDUS_group) %>% 
   # Summarize to calculate the mean tax
@@ -83,7 +83,7 @@ summary_stats
 
 
 # Run a linear regression of tax vs. INDUS_group
-mdl_tax_vs_INDUS <- lm(TAX ~ INDUS_group, data = boston_real_estate)
+mdl_tax_vs_INDUS <- lm(TAX ~ INDUS_group, data = boston_real_estate_clean)
 
 # See the result
 mdl_tax_vs_INDUS
--- 2-r-statistics-fundamentals/2-r-intro-to-regression/02-predictions-and-model-objects/02-predictions-and-model-objects.R
+++ 2-r-statistics-fundamentals/2-r-intro-to-regression/02-predictions-and-model-objects/02-predictions-and-model-objects.R
@@ -1,18 +1,17 @@
 # Install packages 
 install.packages("tidyverse")
-install.packages("dplyr")
-install.packages("magrittr")
-install.packages("ggplot2")
-install.packages("lubridate")
-install.packages("broom")
-
+# install.packages("dplyr")
+# install.packages("magrittr")
+# install.packages("ggplot2")
+# install.packages("lubridate")
+# install.packages("broom")
 
 library(tidyverse) # for data tidying
-library(dplyr) # for tabular data manipulation
-library(magrittr)
-library(ggplot2) # for visualization
-library(lubridate) # for handling date fields
-library(broom)
+# library(dplyr) # for tabular data manipulation
+# library(magrittr)
+# library(ggplot2) # for visualization
+# library(lubridate) # for handling date fields
+library(broom) # for showing results into dataframes
 
 wd <- getwd()
 setwd("C:/Users/sclau/Documents/r-stats-programming-workshop/2-r-statistics-fundamentals/2-r-intro-to-regression/02-predictions-and-model-objects")
@@ -20,20 +19,34 @@ setwd("C:/Users/sclau/Documents/r-stats-programming-workshop/2-r-statistics-fund
 
 boston_real_estate <- read.csv("archive/data.csv")
 
-glimpse(boston_real_estate)
+View(boston_real_estate_clean)
 
-# Create a tibble with index of accessibility to AGEial highways column from zero to 12
+# Remove rows with missing values
+boston_real_estate_clean <- boston_real_estate[complete.cases(boston_real_estate), ]
+
+# Add a linear trend line without a confidence ribbon
+ggplot(boston_real_estate_clean, aes(DIS, NOX)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+# MAKING PREDICTIONS
+
+# Create a tibble with index of accessibility to DIS column from 2 to -2
+# This is also known as the test data 
 explanatory_data <- tibble(
-  AGE = 0:100
+  DIS = seq(0, 2.5, by = 0.1)
 )
 
 
-mdl_TAX_vs_AGE <- lm(TAX ~ AGE + 0, data = boston_real_estate)
+# Create the linear model
+mdl_NOX_vs_DIS <- lm(NOX ~ DIS, data = boston_real_estate_clean)
 
-# Edit this, so predictions are stored in prediction_data
+# Make a prediction of NOX using the model, mdl_NOX_vs_DIS with the new data from explanatory_data
+
 prediction_data <- explanatory_data %>% 
   mutate(
-    TAX = predict(mdl_TAX_vs_AGE, explanatory_data)
+    NOX = predict(mdl_NOX_vs_DIS, explanatory_data)
   )
 
 # See the result
@@ -41,120 +54,138 @@ prediction_data
 
 
 
-# Add to the plot
-ggplot(boston_real_estate, aes(AGE, TAX)) +
+# Plot the predictIon
+ggplot(boston_real_estate_clean, aes(DIS, NOX)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   # Add a point layer of prediction data, colored yellow
   geom_point(data = prediction_data, color = "yellow")
 
 
-coefficients(mdl_TAX_vs_AGE)
 
-fitted(mdl_TAX_vs_AGE)
 
-residuals(mdl_TAX_vs_AGE)
 
-summary(mdl_TAX_vs_AGE)
+# MODEL OBJECTS
+
+# Obtain the vector of coefficients and intercept
+coefficients(mdl_NOX_vs_DIS)
+
+# Obtain the predictions on the original data set
+fitted(mdl_NOX_vs_DIS)
+
+# Obtain the actual - predicted, ⬇️ residuals, ⬆️ model
+residuals(mdl_NOX_vs_DIS)
+
+# Obtain the info summary of the model object
+summary(mdl_NOX_vs_DIS)
+
+# A good fit means it follows a normal distribution
+# Conditions for good fit:
+  # Median is close to 0
+  # Q1 and Q3 are almost equal in terms of their absolute value, IQR ~ 0
+
+
+# broom
+# Get the coefficient-level elements of the model
+# Info for understanding the significance and strength of the relationships between the predictors and the response variable.
+tidy(mdl_NOX_vs_DIS)
+
+# Get the observation-level elements of the model
+# info for analyzing individual observations, detecting outliers, and evaluating model fit
+augment(mdl_NOX_vs_DIS)
+
+# Get the model-level elements of the model
+# Info for assessing the overall performance and fit of the model
+glance(mdl_NOX_vs_DIS)
+
+
 
 
 #MANUAL PREDICTION
 
-# Get the coefficients of mdl_TAX_vs_AGE
-coeffs <- coefficients(mdl_TAX_vs_AGE)
+# Get the coefficients of mdl_NOX_vs_DIS
+coeffs <- coefficients(mdl_NOX_vs_DIS)
+coeffs
 
 # Get the intercept
 intercept <- coeffs[1]
+intercept
 
 # Get the slope
 slope <- coeffs[2]
+slope
 
-explanatory_data %>% 
+
+manual_pred <- explanatory_data %>% 
   mutate(
     # Manually calculate the predictions
-    TAX = intercept + slope * AGE
+    NOX = intercept + slope * DIS
   )
 
-# Compare to the results from predict()
-predict(mdl_TAX_vs_AGE, explanatory_data)
+View(manual_pred)
 
+# Compare to the results from the not manual predict()
+auto_pred <- predict(mdl_NOX_vs_DIS, explanatory_data)
 
-# Get the coefficient-level elements of the model
-tidy(mdl_TAX_vs_AGE)
-
-# Get the observation-level elements of the model
-augment(mdl_TAX_vs_AGE)
-
-# Get the model-level elements of the model
-glance(mdl_TAX_vs_AGE)
+View(auto_pred)
 
 
 
 
-ggplot(boston_real_estate, aes(RAD, TAX)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
 
-# Run the code to see the plot
-# Edit so x-axis is square root of RAD
-ggplot(boston_real_estate, aes(sqrt(RAD), TAX)) +
+
+
+# TRASNFORMING VARIABLES IF THE RELATIONSHIP IS NOT A STRAIHT LINE
+# Plot the original data for comparison
+ggplot(boston_real_estate_clean, aes(DIS, NOX)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
 
-# Run a linear regression of TAX vs. square root of RAD using boston_real_estate
-mdl_TAX_vs_RAD <- lm(
-  TAX ~ sqrt(RAD), 
-  data = boston_real_estate
+# Transform DIS by getting the square root to make the plot more linear
+# SQRT transformation is good for right skewed data, to make the data more distributed
+ggplot(boston_real_estate_clean, aes(sqrt(DIS), NOX)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+# Make a linear regression model of NOX vs. square root of DIS using boston_real_estate_clean
+mdl_NOX_vs_DIS_sqrt <- lm(
+  NOX ~ sqrt(DIS), 
+  data = boston_real_estate_clean
 )
 
-# See the result
-mdl_TAX_vs_RAD
+# Get the model summary
+summary(mdl_NOX_vs_DIS_sqrt)
 
-
-
-# Run a linear regression of TAX vs. square root of RAD using boston_real_estate
-mdl_TAX_vs_RAD <- lm(
-  TAX ~ sqrt(RAD), 
-  data = boston_real_estate
-)
 
 explanatory_data <- tibble(
-  RAD = seq(0, 80, 10) ^ 2
+  DIS = seq(0, 2.5, by = 0.1)
 )
 
-# Use mdl_TAX_vs_RAD to predict explanatory_data
+# Use mdl_NOX_vs_DIS_sqrt to predict explanatory_data_sq
 prediction_data <- explanatory_data %>% 
   mutate(
-    TAX = predict(mdl_TAX_vs_RAD, explanatory_data)
-  )
+    NOX = predict(mdl_NOX_vs_DIS_sqrt, explanatory_data)
+)
 
 # See the result
 prediction_data
 
 
 
-
-
-mdl_TAX_vs_RAD <- lm(
-  TAX ~ sqrt(RAD), 
-  data = boston_real_estate
-)
-
-explanatory_data <- tibble(
-  RAD = seq(0, 20, 10) ^ 2
-)
-
-# Use mdl_TAX_vs_RAD to predict explanatory_data
-prediction_data <- explanatory_data %>% 
-  mutate(
-    TAX = predict(mdl_TAX_vs_RAD, explanatory_data)
-  )
-
-
-
-ggplot(boston_real_estate, aes(sqrt(RAD), TAX)) +
+# Plot the prediction with the transformed independent variable
+ggplot(boston_real_estate_clean, aes(sqrt(DIS), NOX)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
-  # Add points from prediction_data, colored green, size 5
-  geom_point(data = prediction_data, color = "green", size = 5)
+  # Add points from prediction_data, colored green, size 3
+  geom_point(data = prediction_data, color = "green", size = 3)
+
+
+
+# Plot the predition with the original non-transformed independent variable
+ggplot(boston_real_estate_clean, aes((DIS), NOX)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  # Add points from prediction_data, colored vioelt, size 3
+  geom_point(data = prediction_data, color = "violet", size = 3)
